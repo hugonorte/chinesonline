fab3885bbe98ad956179c92e3b5efe96056eec76
Hugo Norte <hugonorte@gmail.com>
2026-09-01 15:42:24 -0300

    feat: add support email address as recipient in contact form submission

--- public/api/contact.php
+++ public/api/contact.php
@@ -111,6 +111,7 @@ try {
     // Remetente e Destinatário
     $mail->setFrom('support@mail.chinesonline.com.br', 'Site Principal ChinesOnline');
     $mail->addAddress('hugonorte@gmail.com'); // Para quem vai o e-mail (você mesmo ou o suporte)
+    $mail->addAddress('support@mail.chinesonline.com.br'); // Cópia para a caixa de entrada do suporte
     $mail->addReplyTo($email, $name); // Se você clicar em 'Responder', vai para o e-mail do cliente
 
     // Conteúdo
